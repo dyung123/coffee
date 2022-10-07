f0e9e6daa47ed37f6d5fbcecd7730b659dfff141
dyung123 <106045803+dyung123@users.noreply.github.com>
2022-10-07 17:56:52 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,67 +1,86 @@
-// 292p
-const badgeEl = document.querySelector('.badges');
+/* 292.p*/
+const badgeEl= document.querySelector('.badges');
+const topBtnEl=document.querySelector('#top_btn');
 
-window.addEventListener('scroll',function(){
-   console.log(window.scrollY); 
-   if(760 <window.scrollY){
+topBtnEl.addEventListener('click',function(){
+    gsap.to(window, .6,{ //브라우저를 선택하여 0.6초만에
+        scrollTo: 0 //스크롤 꼭대기 0까지 올라간다.
+    });
+}); 
+
+window.addEventListener('scroll', function(){
+    console.log(window.scrollY); //스크롤값 확인하기
+    if(760<window.scrollY){
+        // badgeEl.style.display='none';
         gsap.to(badgeEl,0.6,{
             opacity:0,
-            display:'none'   
-        })
-   }
-   else{
-    gsap.to(badgeEl,0.6,{
-        opacity:1,
-        display:'block'
-    })
-   }
+            display:'none'
+        });
+        gsap.to(topBtnEl,0.6,{
+            opacity:1,
+            // x:0
+        });
+    }else{
+        // badgeEl.style.display='block';
+        gsap.to(badgeEl,0.6,{
+            opacity:1,
+            display:'block'
+        });
+        gsap.to(topBtnEl,0.6,{
+            opacity:0,
+            // x:0
+        });
+    }
 });
 
-// gsap.to(요소,시간,속성)
+/*gsap.to(요소,시간,속성)*/ 
 
-const fadeEl = document.querySelectorAll('.fadein');
+const fadeEl=document.querySelectorAll('.fadein');
 
 fadeEl.forEach(function(fadeEl,index){
-    gsap.to(fadeEl,1,{
-        delay:(index+1)*0.7, //0.7초 씩 느리게 시간을 추가
-        opacity:1
-    });
+ gsap.to(fadeEl,1,{
+    delay:(index+1)*0.7,//0.7초씩 느리게 시간을 추가
+    opacity:1
+});
 });
 
-//커피 프로모션 토글 아이콘 클릭시 박스 숨기기/나타내기
+// const promotionEl= document.querySelector("section.promotion");
+// const promotionToggleBtn= document.querySelector(".n_inner__right>.material-icons");
+// //토글 클릭시
+// promotionToggleBtn.addEventListener('click',function(){
+//     if(promotionEl.classList.contains('hide')) {
+//         promotionEl.classList.remove('hide');
+//     }else{
+//         promotionEl.classList.add('hide');
+//     }
+// });
 
-const promotionEL = document.querySelector('section.promotion');
-const toggleBtnEL = document.querySelector('.toggle_btn');
+//커피프로모션 토글아이콘 클릭시 박스 숨기고/ 나타내기
 
+const promotionEl = document.querySelector('section.promotion');
+const toggleBtnEl = document.querySelector('.toggle_btn');
 
-toggleBtnEL.addEventListener('click',function(){
-    if(promotionEL.classList.contains('hide')){
-        //만약 프로모션 클래스에 하이드 서식이 있다면 높이가 0이 되어야함
-        promotionEL.classList.remove('hide');
+//토글버튼을 클릭하면 프로모션의 크기가 변한다.
+
+toggleBtnEl.addEventListener('click',function(){
+    if(promotionEl.classList.contains('hide')){//만약에 promotion클래스에 hide서식이 있다면 조건에 맞으면
+        promotionEl.classList.remove('hide')//높이 663이되어야
+    }else{//그렇지 않으면
+        promotionEl.classList.add('hide')//높이가 0원래대로
     }
-    else{
-        //높이가 다시 원래대로 나와야함
-        promotionEL.classList.add('hide');
-    }
-})
-gsap.to('.m_img1', 1.5, {
-    delay: 1, // 얼마나 늦게 애니메이션을 시작할 것인지 지연 시간을 설정.
-    y: 15, // `transform: translateY(수치);`와 같음. 수직으로 얼마나 움직일지 설정.
-    repeat: -1, // 몇 번 반복하는지를 설정, `-1`은 무한 반복.
-    yoyo: true, // 한번 재생된 애니메이션을 다시 뒤로 재생.
-    ease: Power1.easeInOut // Easing 함수 적용.
-  });
-  gsap.to('.m_img2', 2, {
-    delay: .5,
-    y: 15,
-    repeat: -1,
-    yoyo: true,
-    ease: Power1.easeInOut
-  });
-  gsap.to('.m_img3', 2.5, {
-    delay: 1.5,
-    y: 20,
-    repeat: -1,
-    yoyo: true,
-    ease: Power1.easeInOut
-  });
+});
+
+// 6, 7, 8, 10 컨텐츠가 화면의 80%지점에 보일 때 동작하는 스크립트
+const spyEl = document.querySelectorAll('section.scroll-spy');
+
+spyEl.forEach(function (spyEl) {
+
+    new ScrollMagic
+    .Scene({ //감시할 장면을 추가
+        triggerElement: spyEl,
+        triggerHook: .8 //화면의 80% 지점에서 동작
+    })
+    .setClassToggle(spyEl, 'show') //show클래스를 적용하거나 해제하기
+    .addTo(new ScrollMagic.Controller()); //컨트롤러에 장면을 할당한다
+});
+
